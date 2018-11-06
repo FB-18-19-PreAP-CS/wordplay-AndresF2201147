9346c3ecf231eda17748d2eb197cc626b9c00fe5
Andres Flores <andres06037@gmail.com>
2018-11-06 13:43:21 -0600

    excercise #7 DONE

--- double_letters.py
+++ double_letters.py
@@ -1,6 +1,10 @@
-with open('words.txt') as files:
-    for line in files:
-        for word in line.split():
-            for i in range(len(word) - 5):
-                if word[i] == word[i + 1] and word[i + 2] == word[i + 3] and word[i + 4] == word[i + 5]:
-                    print(word)
+def double():
+    with open('words.txt') as files:
+        for line in files:
+            for word in line.split():
+                for i in range(len(word) - 5):
+                    if word[i] == word[i + 1] and word[i + 2] == word[i + 3] and word[i + 4] == word[i + 5]:
+                        print(word)
+                        
+if __name__ == "__main__":
+    double()
